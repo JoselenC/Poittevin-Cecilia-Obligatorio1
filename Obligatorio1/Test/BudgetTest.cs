5fd1e72aa9ef8fe4d29a06d7baa973fb534c3f5a
Juan Pablo <juanpaps03@gmail.com>
2020-09-22 20:14:30 -0300

    Agregados tests basicos de budget

---
+++ Obligatorio1/Test/BudgetTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class BudgetTest
+    {
+        [TestMethod]
+        public void TestCreateBudgetDate()
+        {
+            DateTime currentDate = DateTime.Now;
+            Budget budget = Budget(currentDate, 4000);
+            Assert.AreEqual(currentDate, budget.date);
+        }
+
+        [TestMethod]
+        public void TestCreateBudgetTotalAmount()
+        {
+            Budget budget = Budget(DateTime.Now, 4000);
+            Assert.AreEqual(4000, budget.totalAmount);
+        }
+    }
+}
