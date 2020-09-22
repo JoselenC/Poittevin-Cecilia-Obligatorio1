b5a473dae3a59c9b1e89486129d8e96f1ffc0d9f
Juan Pablo <juanpaps03@gmail.com>
2020-09-22 20:47:04 -0300

    Agregados nuevos tests a Budget

--- Obligatorio1/Test/BudgetTest.cs
+++ Obligatorio1/Test/BudgetTest.cs
@@ -17,12 +17,48 @@ namespace Test
         }
 
         [TestMethod]
+        public void TestCreateBudgetWithDateInThePass()
+        {
+            DateTime currentDate = DateTime.Now.AddDays(-2);
+            int totalAmount = 4000;
+            Budget budget = new Budget(currentDate, totalAmount);
+            Assert.AreEqual(currentDate, budget.date);
+        }
+        
+        [TestMethod]
+        public void TestCreateBudgetWithDateInTheFeature()
+        {
+            DateTime currentDate = DateTime.Now.AddDays(2);
+            int totalAmount = 4000;
+            Budget budget = new Budget(currentDate, totalAmount);
+            Assert.AreEqual(currentDate, budget.date);
+        }
+
+        [TestMethod]
         public void TestCreateBudgetTotalAmount()
         {
             int totalAmount = 4000;
 
             Budget budget = new Budget(DateTime.Now, totalAmount);
             Assert.AreEqual(4000, budget.totalAmount);
+        }
+
+        [TestMethod]
+        public void TestCreateBudgetWithCeroTotalAmount()
+        {
+            int totalAmount = 0;
+
+            Budget budget = new Budget(DateTime.Now, totalAmount);
+            Assert.AreEqual(0, budget.totalAmount);
+        }
+
+        [TestMethod]
+        public void TestCreateBudgetWithBigTotalAmount()
+        {
+            int totalAmount = int.MaxValue;
+
+            Budget budget = new Budget(DateTime.Now, totalAmount);
+            Assert.AreEqual(int.MaxValue, budget.totalAmount);
         }
     }
 }
