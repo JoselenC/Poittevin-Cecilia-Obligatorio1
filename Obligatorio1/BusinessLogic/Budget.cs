5de216c0bae8355406e387b429d485cfb511fd82
Juan Pablo <juanpaps03@gmail.com>
2020-09-22 20:22:33 -0300

    Agregar logica de budget

---
+++ Obligatorio1/BusinessLogic/Budget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Budget
+    {
+        public int totalAmount { get; set; }
+        public DateTime date { get; set; }
+
+        public Budget(DateTime currentDate, int vtotalAmount)
+        {
+            date = currentDate;
+            totalAmount = vtotalAmount;
+        }
+
+
+    }
+}
