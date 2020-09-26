fd922e3f2aa35a4bdaad2d8c8caf3628e9194a19
Juan Pablo <juanpaps03@gmail.com>
2020-09-26 02:03:58 -0300

    Hacer Category requerido en el BudgetCategori

--- Obligatorio1/BusinessLogic/BudgetCategory.cs
+++ Obligatorio1/BusinessLogic/BudgetCategory.cs
@@ -1,14 +1,22 @@
-﻿namespace BusinessLogic
+﻿using System;
+
+namespace BusinessLogic
 {
     public class BudgetCategory
     {
+        public Category category { get => category; set => SetCategory(value); }
+
+        private void SetCategory(Category vCategory)
+        {
+            category = vCategory ?? throw new ArgumentNullException();
+        }
+
+        public double amount { get; }
         public BudgetCategory(Category vCategory, double vAmount)
         {
             category = vCategory;
             amount = vAmount;
         }
 
-        public Category category { get; }
-        public double amount { get; }
     }
 }
