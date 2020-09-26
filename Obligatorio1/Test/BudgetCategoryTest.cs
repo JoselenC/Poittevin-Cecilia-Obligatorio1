a2b5109e9cc932237cf991718ce15249c988ac6b
Juan Pablo <juanpaps03@gmail.com>
2020-09-26 00:54:39 -0300

    Agregar budgetCategory tests

---
+++ Obligatorio1/Test/BudgetCategoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class BudgetCategoryTest
+    {
+        [TestMethod]
+        public void TestCreateBudgetCategory()
+        {
+            Category category = new Category("testCategory");
+            double amount = 100;
+            new BudgetCategory(category, amount);
+        }
+
+        [TestMethod]
+        public void TestCreateBudgetCategoryNullCategory()
+        {
+            double amount = 100;
+            new BudgetCategory(null, amount);
+        }
+
+        [TestMethod]
+        public void TestCreateBudgetCategoryAmountWithDecimals()
+        {
+            double amount = 100.23;
+            new BudgetCategory(null, amount);
+        }
+    }
+}
