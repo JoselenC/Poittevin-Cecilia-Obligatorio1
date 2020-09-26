4aec210962e002069ff7a72284a8a39929906ad2
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-09-26 00:41:17 -0300

    Se modifica el metodo creatExpense agregando description y un assert

--- Obligatorio1/Test/ExpenseTest.cs
+++ Obligatorio1/Test/ExpenseTest.cs
@@ -157,10 +157,12 @@ namespace Test
         public void creatExpense()
         {
             int amount = 23;
+            string description = "cuando fui al cine";
             DateTime creationDate = new DateTime(2020, 01, 01);
-            Expense expense = new Expense(amount, creationDate);
+            Expense expense = new Expense(amount, creationDate,description);
             Assert.AreEqual(expense.amount, amount);
             Assert.AreEqual(expense.creationDate, creationDate);
+            Assert.AreEqual(expense.description, description);
 
         }
     }
