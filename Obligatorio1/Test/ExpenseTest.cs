69aa4bbb4e3f3249e56b57829a4f0f94101699bf
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-09-26 00:52:54 -0300

    Se agrega a la clase test el metodo creatExpenseInvalidDescription2

--- Obligatorio1/Test/ExpenseTest.cs
+++ Obligatorio1/Test/ExpenseTest.cs
@@ -63,96 +63,13 @@ namespace Test
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateMonth()
+        [ExpectedException(typeof(InvalidOperationException), "")]
+        public void creatExpenseInvalidDescription2()
         {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, 31, 01);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateMonth2()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, 0, 01);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateMonth3()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, -1, 01);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateDay()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, 10, 40);
-            Expense expense = new Expense(amount, creationDate);
-            Assert.Fail();
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateDay2()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, 10, 0);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDateDay3()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2020, 10, -1);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-     [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDate()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2030, 35, 40);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        //February cannot have thirty days
-        public void creatExpenseInvalidDate2()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2030, 2, 30);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        //April cannot have fthirty one days
-        public void creatExpenseInvalidDate3()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2030, 4, 31);
-            Expense expense = new Expense(amount, creationDate);
-        }
-
-         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "")]
-        public void creatExpenseInvalidDate4()
-        {
-            int amount = 23;
-            DateTime creationDate = new DateTime(2030, 1, 32);
-            Expense expense = new Expense(amount, creationDate);
+            double amount = 23;
+            string description = "cuando fuimos al cine de punta carretas";
+            DateTime creationDate = new DateTime(2021, 2, 2);
+            Expense expense = new Expense(amount, creationDate, description);
         }
 
         [TestMethod]
