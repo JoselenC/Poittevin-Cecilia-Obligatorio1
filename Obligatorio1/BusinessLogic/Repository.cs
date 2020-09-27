5afdab8fb4b66f5ee23d0e9fc06d31d6b35ddf10
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-09-26 23:00:00 -0300

    Se crea la clase repositorio con el constructor y el metodo findCategory

---
+++ Obligatorio1/BusinessLogic/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Repository
+    {
+        public Repository(List<Category> categoryList)
+        {
+
+        }
+
+        public Category findCategory(String description)
+        {
+              return null;
+        }
+
+    }
+}
