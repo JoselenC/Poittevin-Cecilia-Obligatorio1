f438df878d8d041959f3b5d23e307f0c4eddf5f6
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-09-28 14:28:45 -0300

    Se agrega a la clase repositoryTest el metodo findCategory2

--- Obligatorio1/Test/repositoryTest.cs
+++ Obligatorio1/Test/repositoryTest.cs
@@ -31,6 +31,25 @@ namespace Test
              
         }
 
+        [TestMethod]
+        public void findCategory2()
+        {
+            List<Category> categoryList = new List<Category>();
+            List<string> keyWords1 = new List<string>();
+            keyWords1.Add("cena");
+            keyWords1.Add("teatro");
+            keyWords1.Add("casino");
+            Category category1 = new Category("entretenimiento", keyWords1);
+            List<string> keyWords2 = new List<string>();
+            keyWords2.Add("restaurante");
+            keyWords2.Add("McDonalds");
+            keyWords2.Add("cena");
+            Category category2 = new Category("comida", keyWords2);
+            categoryList.Add(category1);
+            categoryList.Add(category2);
+            Repository repo = new Repository();
+            string description = "cuando fuimos a la cena";
+            Assert.IsNull(repo.findCategory(description, categoryList));
             Category category2 = new Category("comida", keyWords2);
             categoryList.Add(category1);
             categoryList.Add(category2);
