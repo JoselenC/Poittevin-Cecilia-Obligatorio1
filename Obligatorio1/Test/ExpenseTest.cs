b5ff5ffaff696f2acbaf1bdcb65622f2eac74f3c
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-05 08:24:08 -0300

    Se modifica los metodos de expenseTest
    Se agrega en estos metodos,, una lista de categorias y una instancia de repositorio para asignar lacategoria

--- Obligatorio1/Test/ExpenseTest.cs
+++ Obligatorio1/Test/ExpenseTest.cs
@@ -16,9 +16,22 @@ namespace Test
 
             double amount = 0;
             string description = "";
-            DateTime creationDate = new DateTime(2020, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate,description,category);
+            DateTime creationDate = new DateTime(2020, 01, 01); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
 
         }
 
@@ -30,9 +43,22 @@ namespace Test
 
             double amount = -10.5;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(2020, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2020, 01, 01); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -43,9 +69,22 @@ namespace Test
 
             double amount = 23.555;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(2020, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2020, 01, 01); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -56,9 +95,23 @@ namespace Test
 
             double amount = 23.344;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(2020, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2020, 01, 01); 
+            String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -67,9 +120,22 @@ namespace Test
         {
             double amount = 23;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(2031, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2031, 01, 01); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -78,9 +144,22 @@ namespace Test
         {
             double amount = 23;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(2017, 2, 2);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2017, 2, 2); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -89,9 +168,22 @@ namespace Test
         {
             double amount = 23;
             string description = "cuando fui al cine";
-            DateTime creationDate = new DateTime(0, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(0, 01, 01); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -100,9 +192,22 @@ namespace Test
         {
             double amount = 23;
             string description = "cuando fuimos al cine de punta carretas";
-            DateTime creationDate = new DateTime(2021, 2, 2);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
+            DateTime creationDate = new DateTime(2021, 2, 2); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -111,8 +216,22 @@ namespace Test
         {
             double amount = 23;
             string description = "aa";
-            DateTime creationDate = new DateTime(2021, 2, 2);
-            Expense expense = new Expense(amount, creationDate, description);
+            DateTime creationDate = new DateTime(2021, 2, 2); String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
         }
 
         [TestMethod]
@@ -121,12 +240,27 @@ namespace Test
             double amount = 23.55;
             string description = "cuando fui al cine";
             DateTime creationDate = new DateTime(2020, 01, 01);
-            Category category = repository.findCategory(description);
-            Expense expense = new Expense(amount, creationDate, description, category);
-            Assert.AreEqual(expense.amount, amount);
-            Assert.AreEqual(expense.creationDate, creationDate);
-            Assert.AreEqual(expense.description, description);
-            Assert.AreEqual(expense.category, category);
+            String categoryName = "entretenimiento";
+            List<string> keyWords = new List<string>();
+            keyWords.Add("cine");
+            keyWords.Add("teatro");
+            keyWords.Add("salida");
+            keyWords.Add("libreria");
+            keyWords.Add("jugeteria");
+            keyWords.Add("shopping");
+            keyWords.Add("patinaje");
+            keyWords.Add("casino");
+            keyWords.Add("sala de juego");
+            Category category = new Category(categoryName, keyWords);
+            List<Category> categories = new List<Category>();
+            categories.Add(category);
+            Repository repository = new Repository(categories);
+            Expense expense = new Expense(amount, creationDate, description, repository);
+            Assert.AreEqual(expense.Amount, amount);
+            Assert.AreEqual(expense.CreationDate, creationDate);
+            Assert.AreEqual(expense.Description, description);
+            Assert.AreEqual(expense.Category, category);
+        }
 
         }
 
