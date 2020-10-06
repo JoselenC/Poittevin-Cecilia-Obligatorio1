438225901602fdc885f23c44e1753232918c8fde
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-06 18:35:40 -0300

    Se agregan en la clase Expense las excepciones propias

--- Obligatorio1/BusinessLogic/Expense.cs
+++ Obligatorio1/BusinessLogic/Expense.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -16,42 +16,38 @@ namespace BusinessLogic
 
         public Category Category { get; set; }
 
-        private bool ValidDate(DateTime date)
+        private void ValidDate(DateTime date)
         {
-            if (date.Year > 2030 || date.Year < 2018) { return false; }
-            return true;
+            if (date.Year > 2030 || date.Year < 2018)
+                throw new ExcepcionInvalidYearExpense();
         }
 
-        private bool ValidAmount(double amountPassed)
+        private void ValidAmount(double amountPassed)
         {
-            if (amountPassed <= 0) { return false; }
-            if (amountPassed != Math.Round(amountPassed, 2)) { return false; }
-            else { return true; }
+            if (amountPassed <= 0)
+                throw new ExcepcionNegativeAmountExpense();
+
+            if (amountPassed != Math.Round(amountPassed, 2))
+                throw new ExcepcionInvalidAmountExpense();
         }
 
-        private bool ValidDescription(string description)
+        private void ValidDescription(string description)
         {
-            if (description.Length < 3 || description.Length > 20) { return false; }
-            else { return true; }
+            if (description.Length < 3 || description.Length > 20)
+                throw new ExcepcionInvalidDescriptionLengthExpense();            
         }
                
         public Expense(double amountPassed, DateTime creationDatePassed, string descriptionPassed)
         {
-            if (!ValidAmount(amountPassed) || !ValidDescription(descriptionPassed))
-            {
-                throw new InvalidOperationException();
-            }
-            else if (!ValidDate(creationDatePassed))
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-            else
-            {
-                this.Amount = amountPassed;
-                this.CreationDate = creationDatePassed;
-                this.Description = descriptionPassed;
-                this.Category = null;
-            }
+            ValidAmount(amountPassed);
+            ValidDescription(descriptionPassed);          
+            ValidDate(creationDatePassed);            
+            this.Amount = amountPassed;
+            this.CreationDate = creationDatePassed;
+            this.Description = descriptionPassed;
+            this.Category = null;
+
+            
         }
 
     }
