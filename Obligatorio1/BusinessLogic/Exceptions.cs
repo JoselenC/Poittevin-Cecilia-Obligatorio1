81a991fc786113af5cb12c3415dc6f97fa793064
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-06 18:37:56 -0300

    Se crea la agre Exceptions con las excepciones creadas pornosotros

---
+++ Obligatorio1/BusinessLogic/Exceptions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class Exceptions
+    {
+    }
+
+    public class ExcepcionInvalidNameLengthCategory : Exception
+    {
+    }
+
+    public class ExcepcionInvalidNameDigitCategory : Exception
+    {
+    }
+
+    public class ExcepcionInvalidKeyWordsLengthCategory : Exception
+    {
+    }
+
+    public class ExcepcionInvalidRepeatedNameCategory : Exception
+    {
+    }
+
+    public class ExcepcionInvalidRepeatedKeyWordsCategory : Exception
+    {
+    }
+
+    public class ExcepcionInvalidYearExpense : Exception
+    {
+    }
+
+    public class ExcepcionNegativeAmountExpense : Exception
+    {
+    }
+
+    public class ExcepcionInvalidAmountExpense : Exception
+    {
+    }
+
+    public class ExcepcionInvalidDescriptionLengthExpense : Exception
+    {
+    }
+
+    public class ExcepcionExpenseWithEmptyCategory : Exception
+    {
+    }
+}
