d37dac450c9bfc7bf5fa284595594fc7d89192fb
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-08 16:31:06 -0300

    Se crea el gasto cuando se clickea en el boto register Expense

--- Obligatorio1/InterfazLogic/RegisterExpense.Designer.cs
+++ Obligatorio1/InterfazLogic/RegisterExpense.Designer.cs
@@ -34,15 +34,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.tbDescription = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
             this.lbDescription = new System.Windows.Forms.Label();
             this.lstCategories = new System.Windows.Forms.ListBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.nAmount = new System.Windows.Forms.NumericUpDown();
             this.lblAmount = new System.Windows.Forms.Label();
             this.lblDate = new System.Windows.Forms.Label();
             this.lblCategories = new System.Windows.Forms.Label();
             this.btnRegistrExpense = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAmount)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -104,13 +104,13 @@
             this.tbDescription.Size = new System.Drawing.Size(451, 38);
             this.tbDescription.TabIndex = 5;
             // 
-            // dateTimePicker1
+            // dateTime
             // 
-            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.OldLace;
-            this.dateTimePicker1.Location = new System.Drawing.Point(333, 359);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(447, 38);
-            this.dateTimePicker1.TabIndex = 6;
+            this.dateTime.CalendarMonthBackground = System.Drawing.Color.OldLace;
+            this.dateTime.Location = new System.Drawing.Point(333, 359);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(447, 38);
+            this.dateTime.TabIndex = 6;
             // 
             // lbDescription
             // 
@@ -131,30 +131,30 @@
             this.lstCategories.Size = new System.Drawing.Size(446, 35);
             this.lstCategories.TabIndex = 8;
             // 
-            // numericUpDown1
+            // nAmount
             // 
-            this.numericUpDown1.BackColor = System.Drawing.Color.OldLace;
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Increment = new decimal(new int[] {
+            this.nAmount.BackColor = System.Drawing.Color.OldLace;
+            this.nAmount.DecimalPlaces = 2;
+            this.nAmount.Increment = new decimal(new int[] {
             1,
             0,
             0,
             65536});
-            this.numericUpDown1.Location = new System.Drawing.Point(336, 261);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.nAmount.Location = new System.Drawing.Point(336, 261);
+            this.nAmount.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.nAmount.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(268, 38);
-            this.numericUpDown1.TabIndex = 9;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.nAmount.Name = "nAmount";
+            this.nAmount.Size = new System.Drawing.Size(268, 38);
+            this.nAmount.TabIndex = 9;
+            this.nAmount.Value = new decimal(new int[] {
             1,
             0,
             0,
@@ -196,6 +196,7 @@
             this.btnRegistrExpense.TabIndex = 13;
             this.btnRegistrExpense.Text = "RegisterExpense";
             this.btnRegistrExpense.UseVisualStyleBackColor = false;
+            this.btnRegistrExpense.Click += new System.EventHandler(this.btnRegistrExpense_Click);
             // 
             // RegisterExpense
             // 
@@ -206,10 +207,10 @@
             this.Controls.Add(this.lblCategories);
             this.Controls.Add(this.lblDate);
             this.Controls.Add(this.lblAmount);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.nAmount);
             this.Controls.Add(this.lstCategories);
             this.Controls.Add(this.lbDescription);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTime);
             this.Controls.Add(this.tbDescription);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -218,7 +219,7 @@
             this.Controls.Add(this.label1);
             this.Name = "RegisterExpense";
             this.Size = new System.Drawing.Size(1120, 636);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAmount)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,10 +233,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbDescription;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTime;
         private System.Windows.Forms.Label lbDescription;
         private System.Windows.Forms.ListBox lstCategories;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown nAmount;
         private System.Windows.Forms.Label lblAmount;
         private System.Windows.Forms.Label lblDate;
         private System.Windows.Forms.Label lblCategories;
--- Obligatorio1/InterfazLogic/RegisterExpense.cs
+++ Obligatorio1/InterfazLogic/RegisterExpense.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
 
 namespace InterfazLogic
 {
     public partial class RegisterExpense : UserControl
     {
-        public RegisterExpense()
+        private Repository repository;
+        public RegisterExpense(Repository vRepository)
         {
             InitializeComponent();
+            repository = vRepository; e
+        }
+
+
+
+        private void btnRegistrExpense_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string description = tbDescription.Text;
+                double amount = decimal.ToDouble(nAmount.Value);
+                DateTime creationDate = dateTime.Value;
+                Expense expense = new Expense( amount, creationDate,description);
+                Category category=repository.FindCategoryByDescription(description)
+                if (expense.Category == null && repository.Categories.Count>0)
+                {
+                    CompleteCategories();
+                    string categoryName= lstCategories.SelectedItem.ToString();
+                    category = repository.findCategoryByNAme(categoryName);
+                }
+               
+                repository.addExpense(expense);
+                lbDescription.Text = "";
+            }
+            catch (ExcepcionInvalidDescriptionLengthExpense)
+            {
+                lblErrorDescription.Text = "The name must be between 3 and 20 characters long.";
+
+            }
+            catch (ExcepcionNegativeAmountExpense)
+            {
+                lblErrorDescription.Text = "The amount must be positive";
+            }
+            catch (ExcepcionInvalidYearExpense)
+            {
+                lblErrorDescription.Text = "The year must be between 2018 and 2030.";
+            }
+
         }
     }
 }
