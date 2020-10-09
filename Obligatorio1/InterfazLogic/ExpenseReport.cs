fae988f336bf4e31161a92404f220f810d9bcf8f
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-09 08:01:20 -0300

    Se agrega el user control Expense report y el evento btnConsult_Click

---
+++ Obligatorio1/InterfazLogic/ExpenseReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace InterfazLogic
+{
+    public partial class ExpenseReport : UserControl
+    {
+        private Repository repository;
+        public ExpenseReport(Repository vRepository)
+        {
+            InitializeComponent();
+            repository = vRepository;
+            monthsWithExpenses();
+
+        }
+
+        private void monthsWithExpenses(){
+            List <string> months= repository.MonthsOrdered();
+            for (int i = 0; i < months.Count; i++)
+            {
+                lstMonths.Items.Add(months[i]);
+            }
+        }
+
+
+        private void btnConsult_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
