16d077b23805fd260f72c84ab9a9f32097f4902c
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-10-11 22:42:37 -0300

    Se descomenta el codigo de btnRegisterCategory

--- Obligatorio1/InterfazLogic/Menu.cs
+++ Obligatorio1/InterfazLogic/Menu.cs
@@ -22,16 +22,16 @@ namespace InterfazLogic
 
         private void btnRegisterCategory_Click(object sender, EventArgs e)
         {
-           // mainPanel.Controls.Clear();
-           // UserControl registerCategory = new RegisterCategory(repository);
-           // mainPanel.Controls.Add(registerCategory);
+            mainPanel.Controls.Clear();
+           UserControl registerCategory = new RegisterCategory(repository);
+            mainPanel.Controls.Add(registerCategory);
         }
 
         private void btnRegisterExpense_Click(object sender, EventArgs e)
         {
-           // mainPanel.Controls.Clear();
-           // UserControl registerCategory = new RegisterCategory(repository);
-           // mainPanel.Controls.Add(registerCategory);
+           mainPanel.Controls.Clear();
+           UserControl registerExpense = new RegisterExpense(repository);
+            mainPanel.Controls.Add(registerExpense);
         }
 
         private void btnExpenseReport_Click(object sender, EventArgs e)
