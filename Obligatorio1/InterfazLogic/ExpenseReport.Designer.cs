4d87c4b31370a0219ac2c36edec5d65bb2fd3970
Joselen Cecilia <JC233552@fi365.ort.edu.uy>
2020-11-02 21:29:37 -0300

    Se agrega la nueva funcionalidad que permite exportar archivos

--- Obligatorio1/InterfazLogic/ExpenseReport.Designer.cs
+++ Obligatorio1/InterfazLogic/ExpenseReport.Designer.cs
@@ -35,28 +35,32 @@
             this.cCreationDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cMoney = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cAmount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lblMonths = new System.Windows.Forms.Label();
             this.lblMonth = new System.Windows.Forms.Label();
             this.btnAccept = new System.Windows.Forms.Button();
             this.lblTotalAmount = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.lstType = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lstMonths
             // 
             this.lstMonths.FormattingEnabled = true;
             this.lstMonths.ItemHeight = 31;
-            this.lstMonths.Location = new System.Drawing.Point(345, 137);
+            this.lstMonths.Location = new System.Drawing.Point(333, 148);
             this.lstMonths.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.lstMonths.Name = "lstMonths";
-            this.lstMonths.Size = new System.Drawing.Size(457, 66);
+            this.lstMonths.Size = new System.Drawing.Size(457, 35);
             this.lstMonths.TabIndex = 1;
             // 
             // Month
             // 
             this.Month.AutoSize = true;
-            this.Month.Location = new System.Drawing.Point(187, 137);
+            this.Month.Location = new System.Drawing.Point(190, 151);
             this.Month.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.Month.Name = "Month";
             this.Month.Size = new System.Drawing.Size(108, 32);
@@ -66,7 +70,7 @@
             // btnConsult
             // 
             this.btnConsult.BackColor = System.Drawing.Color.DarkKhaki;
-            this.btnConsult.Location = new System.Drawing.Point(839, 137);
+            this.btnConsult.Location = new System.Drawing.Point(817, 137);
             this.btnConsult.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnConsult.Name = "btnConsult";
             this.btnConsult.Size = new System.Drawing.Size(219, 56);
@@ -84,10 +88,10 @@
             this.cCategory,
             this.cAmount});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(142, 270);
+            this.listView1.Location = new System.Drawing.Point(22, 369);
             this.listView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(945, 381);
+            this.listView1.Size = new System.Drawing.Size(1163, 381);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -107,6 +111,12 @@
             this.cCategory.Text = "Category";
             this.cCategory.Width = 90;
             // 
+            // cMoney
+            // 
+            this.cMoney.DisplayIndex = 4;
+            this.cMoney.Text = "Money";
+            this.cMoney.Width = 90;
+            // 
             // cAmount
             // 
             this.cAmount.Text = "Amount";
@@ -115,7 +125,7 @@
             // lblMonths
             // 
             this.lblMonths.AutoSize = true;
-            this.lblMonths.Location = new System.Drawing.Point(187, 217);
+            this.lblMonths.Location = new System.Drawing.Point(76, 218);
             this.lblMonths.Name = "lblMonths";
             this.lblMonths.Size = new System.Drawing.Size(0, 32);
             this.lblMonths.TabIndex = 5;
@@ -123,7 +133,7 @@
             // lblMonth
             // 
             this.lblMonth.AutoSize = true;
-            this.lblMonth.Location = new System.Drawing.Point(192, 207);
+            this.lblMonth.Location = new System.Drawing.Point(81, 217);
             this.lblMonth.Name = "lblMonth";
             this.lblMonth.Size = new System.Drawing.Size(0, 32);
             this.lblMonth.TabIndex = 6;
@@ -131,7 +141,7 @@
             // btnAccept
             // 
             this.btnAccept.BackColor = System.Drawing.Color.Tan;
-            this.btnAccept.Location = new System.Drawing.Point(485, 747);
+            this.btnAccept.Location = new System.Drawing.Point(499, 875);
             this.btnAccept.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnAccept.Name = "btnAccept";
             this.btnAccept.Size = new System.Drawing.Size(227, 73);
@@ -143,7 +153,7 @@
             // lblTotalAmount
             // 
             this.lblTotalAmount.AutoSize = true;
-            this.lblTotalAmount.Location = new System.Drawing.Point(64, 658);
+            this.lblTotalAmount.Location = new System.Drawing.Point(48, 777);
             this.lblTotalAmount.Name = "lblTotalAmount";
             this.lblTotalAmount.Size = new System.Drawing.Size(0, 32);
             this.lblTotalAmount.TabIndex = 8;
@@ -161,11 +171,46 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "              Expense report              ";
             // 
+            // btnExportar
+            // 
+            this.btnExportar.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnExportar.Location = new System.Drawing.Point(817, 259);
+            this.btnExportar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(219, 56);
+            this.btnExportar.TabIndex = 10;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = false;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // lstType
+            // 
+            this.lstType.FormattingEnabled = true;
+            this.lstType.ItemHeight = 31;
+            this.lstType.Location = new System.Drawing.Point(333, 270);
+            this.lstType.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.lstType.Name = "lstType";
+            this.lstType.Size = new System.Drawing.Size(457, 35);
+            this.lstType.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 272);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 32);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Archive";
+            // 
             // ExpenseReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OldLace;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstType);
+            this.Controls.Add(this.btnExportar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblTotalAmount);
             this.Controls.Add(this.btnAccept);
@@ -177,7 +222,7 @@
             this.Controls.Add(this.lstMonths);
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "ExpenseReport";
-            this.Size = new System.Drawing.Size(1209, 863);
+            this.Size = new System.Drawing.Size(1209, 983);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,5 +242,9 @@
         private System.Windows.Forms.Button btnAccept;
         private System.Windows.Forms.Label lblTotalAmount;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader cMoney;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.ListBox lstType;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Obligatorio1/InterfazLogic/ExpenseReport.cs
+++ Obligatorio1/InterfazLogic/ExpenseReport.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using BusinessLogic;
+using System.IO;
+using System;
 
 namespace InterfazLogic
 {
     public partial class ExpenseReport : UserControl
     {
         private ExpenseController expenseController;
+
+        public List<Expense> expenseReportByMonth;
         public ExpenseReport(MemoryRepository vRepository)
         {
             InitializeComponent();
@@ -16,6 +20,11 @@ namespace InterfazLogic
             monthsWithExpenses();
             MaximumSize = new Size(500, 650);
             MinimumSize = new Size(500, 650);
+            btnExportar.Enabled = false;
+            lstType.Items.Add("TXT");
+            lstType.Items.Add("CSV");
+            lstType.SelectedIndex = 0;
+            lstMonths.SelectedIndex = 0;
         }
 
         private void monthsWithExpenses(){
@@ -48,7 +57,7 @@ namespace InterfazLogic
                     lblMonths.Text = "You must select a month to consult";
                     lblMonth.ForeColor = Color.Red;
                 }
-                List<Expense> expenseReportByMonth = expenseController.GetExpenseByMonth(month);
+                expenseReportByMonth = expenseController.GetExpenseByMonth(month);
                 listView1.Items.Clear();
                 ListViewItem item = new ListViewItem();
                 foreach(Expense vExpense in expenseReportByMonth)
@@ -71,7 +80,7 @@ namespace InterfazLogic
                 lblMonths.Text = "Select a month to consult";
                 lblMonth.ForeColor = Color.Red;
             }
-
+            btnExportar.Enabled = true;
         }
 
         private void btnAccept_Click(object sender, EventArgs e)
@@ -79,6 +88,52 @@ namespace InterfazLogic
            Visible = false;
         }
 
-      
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            if(lstType.SelectedItem.ToString()=="TXT")
+                GenerarTXT();
+            else if (lstType.SelectedItem.ToString() =="CSV")
+                GenerarCSV();
+        }
+
+        void GenerarTXT()
+        {
+                    
+            Archive archive = new Archive();
+            string name = archive.Name;
+            string route = archive.Route;
+            string rutaCompleta = @" D:\" + name + ".txt";
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                foreach (Expense vExpense in expenseReportByMonth)
+                {
+                    mylogs.WriteLine(vExpense.CreationDate.ToString("dd/MM/yyyy"));
+                    mylogs.WriteLine(vExpense.Description);
+                    mylogs.WriteLine(vExpense.Category.Name);
+                    mylogs.WriteLine(vExpense.Money.Symbol);
+                    mylogs.WriteLine(vExpense.Amount.ToString());
+                    mylogs.WriteLine("####");
+                }
+
+            }
+        }
+
+        void GenerarCSV()
+        {
+
+            Archive archive = new Archive();
+            string name = archive.Name;
+            string route = archive.Route;
+            string rutaCompleta = @" D:\" + name + ".csv";
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                foreach (Expense vExpense in expenseReportByMonth)
+                {
+                    mylogs.WriteLine(vExpense.CreationDate.ToString("dd/MM/yyyy")+","+ vExpense.Description+","+
+                        vExpense.Category.Name+","+ vExpense.Money.Symbol+","+ vExpense.Amount.ToString());
+                }
+
+            }
+        }
     }
 }
