eca5dec9f7df7eaaf6a4f763bac929c62b7acb3f
Joselen Cecilia <choche_cr@hotmail.com>
2020-11-23 15:53:54 -0300

    Se agregan clases para el manejo del export report

---
+++ Obligatorio1/BusinessLogic/Domain/StrategyExportReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Domain
+{
+    public class StrategyExportReport
+    {
+        public void ExportReport(List<Expense> expenses, string fileName)
+        {
+
+        }
+
+    }
+}
